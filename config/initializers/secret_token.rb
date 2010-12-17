89598fd27a48ac6c5df8f3dcb018662fd22771e4
Mike Lewis <ft.mikelewis@gmail.com>
2010-12-16 19:45:18 -0500

    init commit for grubulate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grubulate::Application.config.secret_token = 'f78ea87fbf44630e3f6503f018a930ab8fb4be604fb35a734427e6fbfca14e9933388be1d7576442ee9dc1b9562a5d2b68d124f9ba4284bbc18c8eebb34b3747'
